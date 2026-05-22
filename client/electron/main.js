41868eeb8dfec41316941e432cd24b809ac0f6d5
Mahidharchowdary2004 <mahidharchowdary2004@users.noreply.github.com>
2026-05-22 14:01:59 +0530

    l

---
+++ client/electron/main.js
@@ -0,0 +1,75 @@
+import { app, BrowserWindow, shell } from 'electron'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// Ensure app is a single instance
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
+
+let win = null
+
+const preload = join(__dirname, 'preload.js')
+const indexHtml = join(__dirname, '../dist/index.html')
+
+function createWindow() {
+  win = new BrowserWindow({
+    title: 'Restauraq POS',
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload,
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) { // vite-plugin-electron sets this
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    // Open devTools, see https://github.com/electron/electron/issues/12438 for why we wait for dom-ready
+    win.webContents.on('did-finish-load', () => {
+      win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
+  } else {
+    win.loadFile(indexHtml)
+  }
+
+  // Test actively push message to the Electron-Renderer
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  })
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith('https:')) shell.openExternal(url)
+    return { action: 'deny' }
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  win = null
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+  if (win) {
+    // Focus on the main window if the user tried to open another
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows()
+  if (allWindows.length) {
+    allWindows[0].focus()
+  } else {
+    createWindow()
+  }
+})
